5fa393fbf61e109b8852e89d140b20ae42445590
nbdd0121 <nbdd0121@hotmail.com>
2014-08-05 19:53:00 -0700

    Add VFS abstract layer

--- js/terminal.js
+++ js/terminal.js
@@ -1,147 +1,4 @@
-(function(window, $){
-	var root={
-		__path:""
-	};
-	root.__parent=root;
-	var term={};
-	
-	term.stdout={
-		color:'class="color37 color40"',
-		write:function write(data){
-			var that=this;
-			function genCode(str){
-				var seq=str.split("\033[");
-				var ret;
-				if(seq[0]){
-					ret=$('<span '+term.stdout.color+'>');
-					ret.text(seq[0]);
-				}else{
-					ret=$();
-				}
-				for(var seqNum=1;seqNum<seq.length;seqNum++){
-					var txt=seq[seqNum];
-					var realTextBegin=txt.indexOf("m");
-					var colorCode=txt.substr(0, realTextBegin).split(";");
-					if(colorCode=="0"){
-						term.stdout.color='class="color37 color40"';
-					}else{
-						term.stdout.color='class="color'+colorCode[1]+' color'+colorCode[2]+'"';
-					}
-					var jq=$('<span '+term.stdout.color+'>');
-					jq.text(txt.substr(realTextBegin+1));
-					ret=ret.add(jq);
-				}
-				return ret;
-			}
-			var datas=data.split("\n");
-			$("p:last-child").append(genCode(datas[0]));
-			for(var lineNum=1;lineNum<datas.length;lineNum++){
-				var line=datas[lineNum];
-				var prevp=$("p:last-child");
-				if(prevp.text()=="")prevp.html("&nbsp;");
-				$("body").append("<p>");
-				$("p:last-child").append(genCode(line));
-			}
-		}
-	};
-	
-	term.stdin={
-		readLine:function readLine(callback, funckey){
-			function adjustWidth(input){
-				input.width($("body").width()-input.offset().left);
-			}
-			var input=$('<input id="inputbox" '+term.stdout.color+' spellcheck=false />');
-			$("p:last-child").append(input);
-			adjustWidth(input);
-			input.focus();
-			input.keypress(function(e){
-				if(e.which==13){
-					input.remove();
-					term.stdout.write(input.val()+"\n");
-					callback(input.val());
-				}
-			});
-			input.keydown(function(e){
-				switch(e.which){
-				case 38:funckey&&funckey("up", input);break;
-				case 40:funckey&&funckey("down", input);break;
-				}
-			});
-		}
-	};
-	
-	root.dev={
-	    __parent:root,
-	    __path:"/dev",
-		stdout:{
-			open:function open(){
-				return term.stdout;
-			}
-		},
-		stdin:{
-			open:function open(){
-				return term.stdin;
-			}
-		}
-	};
-	root.home={
-		__parent:root,
-		__path:"/home"
-	};
-	
-	term.lookup=function lookup(fullpath){
-		var path=fullpath.split("/");
-		var currentPath=root;
-		for(var i=1;i<path.length;i++){
-			var subpath=path[i];
-			switch(subpath){
-			case "":case ".":continue;
-			case "..":currentPath=currentPath.__parent;continue;
-			}
-			currentPath=currentPath[subpath];
-			if(currentPath==null)
-				return null;
-		}
-		return currentPath;
-	};
-	
-	term.open=function open(fullpath){
-		return term.lookup(fullpath).open();
-	};
-	
-	term.exec=function exec(fullpath, args, callback){
-		return term.lookup(fullpath).exec(args, callback);
-	};
-	
-	term.create=function create(path){
-		path=path.split("/");
-		var currentPath=root;
-		for(var i=1;i<path.length;i++){
-			if(!currentPath[path[i]]){
-				var newFile={};
-				newFile.__parent=currentPath;
-				newFile.__path=currentPath.__path+"/"+path[i];
-				currentPath[path[i]]=newFile;
-			}
-			currentPath=currentPath[path[i]];
-		}
-		return currentPath;
-	};
-	
-	term.createExecFile=function createExecFile(file, path){
-		var realFile=term.create(path);
-		realFile.__type="exec";
-		realFile.exec=file;
-	};
-	
-	term.env={
-		HOME:"/home/",
-		PATH:"/bin/;;"
-	}
-	
-	window.Root=root;
-	window.Terminal=term;
-})(window, jQuery);
+VFS.lookup("/home/", true);
 
 $(window).load(function(){
 	$("html").click(function(){
@@ -149,92 +6,88 @@ $(window).load(function(){
 	});
 });
 
-Terminal.createExecFile(function(args, callback){
-	var stdout=Terminal.open("/dev/stdout");
+VFS.open("/bin/echo", true).write(function(env, args, callback){
+	var stdout=VFS.open("/dev/stdout");
 	for(var i=1;i<args.length;i++){
 		stdout.write(args[i]+" ");
 	}
 	stdout.write("\n");
 	callback();
-}, "/bin/echo");
+});
 
-Terminal.createExecFile(function(args, callback){
+VFS.open("/bin/clear", true).write(function(env, args, callback){
 	$("body").html("<p>");
 	callback();
-}, "/bin/clear");
+});
 
-Terminal.createExecFile(function(args, callback){
-	var stdout=Terminal.open("/dev/stdout");
-	var stdin=Terminal.open("/dev/stdin");
+VFS.open("/bin/bash", true).write(function(env, args, callback){
+	var stdout=VFS.open("/dev/stdout");
+	var stdin=VFS.open("/dev/stdin");
 	
-	var path=Terminal.env.HOME;
+	var path="/home/";
 	var history=[];
 	var inputBackup;
 	var id=0;
 	
 	var builtinFunc={
-		exit:function exit(args, cbk){
+		exit:function exit(env, args, cbk){
 			callback();
 		},
-		logout:function exit(args, cbk){
+		logout:function exit(env, args, cbk){
 			callback();
 		},
-		cd:function cd(args, cbk){
+		cd:function cd(env, args, cbk){
 			if(!args[1]){
-				path=Terminal.env.HOME;
+				path="/home/"
 			}else{
 				var cddest=args[1][0]=="/"?args[1]:path+args[1];
-				var newPath=Terminal.lookup(cddest);
+				var newPath=VFS.lookup(cddest);
 				if(newPath==null){
 					stdout.write("No such file or directory called "+args[1]+"\n");
-				}else if(newPath.__type){
+				}else if(newPath.type!="dir"){
 					stdout.write(args[1]+" is not a directory\n");
 				}else{
-					path=newPath.__path+"/";
+					path=newPath.path+"/";
 				}
 			}
 			cbk();
 		},
-		pwd:function pwd(args, cbk){
+		pwd:function pwd(env, args, cbk){
 			stdout.write(path+"\n");
 			cbk();
 		},
-		ls:function ls(args, cbk){
-			var current=Terminal.lookup(path);
-			for(var i in current){
-				if(i.substr(0, 2)=="__")continue;
-				stdout.write(i+" ");
+		ls:function ls(env, args, cbk){
+			var current=VFS.open(path).list();
+			for(var i=0;i<current.length;i++){
+				stdout.write(current[i]+" ");
 			}
 			stdout.write("\n");
 			cbk();
 		}
-		
-		
-		
 	};
 	
 	function parseInput(str, callback){
 		str=str.split(" ");
 		if(builtinFunc[str[0]] instanceof Function){
-			builtinFunc[str[0]](str, callback);
+			builtinFunc[str[0]](env, str, callback);
 			return;
 		}
 		if(str[0][0]=="/"){
-			var file=Terminal.lookup(str[0]);
-			if(file&&file.exec){
-				file.exec(str, callback);
+			var file=VFS.open(str[0]);
+			if(file&&file.canExec()){
+				file.exec(env, str, callback);
 				return;
 			}
 			stdout.write(str[0]+" is not supported.\n");
 			callback();
 		}else{
-			var pathsplit=Terminal.env.PATH.split(";");
+			var pathsplit="/bin/;".split(";");
 			pathsplit.push(path);
 			for(var i=0;i<pathsplit.length;i++){
 				if(pathsplit[i]!=""){
-					var file=Terminal.lookup(pathsplit[i]+str[0]);
-					if(file&&file.exec){
-						file.exec(str, callback);
+					var file=VFS.open(pathsplit[i]+str[0]);
+					if(file&&file.canExec()){
+						file.exec(env, str, callback);
 						return;
 					}
 				}
@@ -244,7 +97,7 @@ Terminal.createExecFile(function(args, callback){
 		}
 	}
 	function readLineAndParse(){
-		stdout.write("\033[1;40;34m"+(path==Terminal.env.HOME?"~":path)+" \033[1;40;31m$ \033[0m");
+		stdout.write("\033[1;40;34m"+(path=="/home/"?"~":path)+" \033[1;40;31m$ \033[0m");
 		stdin.readLine(function(val){
 			history.push(val);
 			id=history.length;
@@ -268,17 +121,17 @@ Terminal.createExecFile(function(args, callback){
 				else
 					element.val(history[id]);
 			}
-			
 		});
 	}
 	readLineAndParse();
 }, "/bin/bash");
 
 $(window).load(function(){
-	Terminal.stdout.write("Gary Guo <nbdd0121@hotmail.com>\nCopyright (c) 2014, Gary Guo. All rights reserved.\n");
+	var stdout=VFS.open("/dev/stdout");
+	stdout.write("Gary Guo <nbdd0121@hotmail.com>\nCopyright (c) 2014, Gary Guo. All rights reserved.\n");
 	function createBash(){
-		Terminal.exec("/bin/bash", "/bin/bash", function(){
-			Terminal.stdout.write("Permission Denied: You cannot exit this session.\n");
+		VFS.open("/bin/bash").exec(null, ["/bin/bash"], function(){
+			stdout.write("Permission Denied: You cannot exit this session.\n");
 			createBash();
 		});
 	}
